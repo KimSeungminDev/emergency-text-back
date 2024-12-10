df624af67b579c29db88a5f065f2c24e55e43e06
KimSeungminDev <kimseungmin.dev@gmail.com>
2024-12-10 21:24:49 +0900

    Jar 설정 8 // 백엔드 cors 수정

--- src/main/java/com/springboot/emergencytextback/EmergencyTextBackApplication.java
+++ src/main/java/com/springboot/emergencytextback/EmergencyTextBackApplication.java
@@ -19,7 +19,13 @@ public class EmergencyTextBackApplication {
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         System.setProperty("API_KEY", dotenv.get("API_KEY"));
-        System.setProperty("spring.datasource.url", dotenv.get("DB_HOST"));
+        System.setProperty("spring.datasource.url",
+                String.format("jdbc:mariadb://%s:%s/%s",
+                        dotenv.get("RDS_ENDPOINT"), // RDS 엔드포인트
+                        dotenv.get("DB_PORT"),      // 포트 번호
+                        dotenv.get("DB_NAME")       // 데이터베이스 이름
+                )
+        );
         System.setProperty("external.api.url", dotenv.get("API_URL"));
         System.setProperty("spring.app.url", dotenv.get("SPRING_APP_URL"));
 
