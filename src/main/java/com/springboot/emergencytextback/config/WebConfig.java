aa59dff3c78a5137b4f6aa9f1365172d220cea34
KimSeungminDev <kimseungmin.dev@gmail.com>
2024-12-11 12:41:48 +0900

    Jar 설정 11 // NginX에 맞게 cors 수정3

--- src/main/java/com/springboot/emergencytextback/config/WebConfig.java
+++ src/main/java/com/springboot/emergencytextback/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.springboot.emergencytextback.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${spring.app.url}") // .env에서 값 읽기
     private String springAppUrl;
+
+    public WebConfig() {
+        // .env 파일에서 값 읽어오기
+        Dotenv dotenv = Dotenv.configure().load();
+        this.springAppUrl = dotenv.get("SPRING_APP_URL");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOrigins(springAppUrl+":80") // 환경 변수로 설정된 값
+        // 모든 엔드포인트에 대해 CORS 허용
+        registry.addMapping("/**")
+                .allowedOrigins(springAppUrl + ":80") // 환경 변수로 설정된 값
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 자격증명 포함 요청 허용
